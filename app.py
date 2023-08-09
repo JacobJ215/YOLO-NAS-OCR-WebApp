bc716f1b94b386a34668e8b8ef90dac957dbc934
Jacob Johnson <jacob@Jacobs-MacBook-Pro.local>
2023-08-08 22:28:15 -0400

    added dectection on image to streamlit frontend

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import numpy as np
 import cv2
 from PIL import Image
 import tempfile
+from license_plate_detection import *
 
 
 def main():
@@ -14,6 +15,20 @@ def main():
         confidence = st.sidebar.slider(
             'Confidece', min_value=0.0, max_value=1.0
         )
-
+        img_file = st.sidebar.file_uploader(
+            "upload an Image", type=["png", "jpg", "jpeg"]
+        )
+        demo = "./OCR-2/valid/images/195_jpg.rf.609d4d0dfd006181eb7d52a90c00bfd8.jpg"
+        if img_file is not None:
+            img = cv2.imdecode(np.fromstring(
+                img_file.read(), np.uint8), 1
+            )
+            image = np.array(Image.open(img_file))
+        else:
+            img = cv2.imread(demo)
+            image = np.array(Image.open(demo))
+        st.sidebar.text("Original Image")
+        st.sidebar.image(image)
+        object_detection_ocr(img, confidence)
 if __name__ == '__main__':
     main()
--- license_plate_detection.py
+++ license_plate_detection.py
@@ -1,16 +1,16 @@
 import cv2
 from super_gradients.training import models
 import numpy as np
+import time
 import math
 import easyocr
+import streamlit as st
 
 reader = easyocr.Reader(['en'], gpu=True)
 def ocr_image(frame, x1, y1, x2, y2):
     frame = frame[y1:y2, x1:x2]
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     result = reader.readtext(gray)
-
-    print(result)
     text = ""
     for res in result:
         if len(result) == 1:
@@ -20,45 +20,84 @@ def ocr_image(frame, x1, y1, x2, y2):
     return str(text)
 
 
-cap = cv2.VideoCapture("./Videos/traffic_video.mp4")
-frame_width = int(cap.get(3))
-frame_height = int(cap.get(4))
+def object_detection_ocr(image, confidence):
+    device = "cpu"
+    model = models.get('yolo_nas_s', 
+                        num_classes=1, 
+                        checkpoint_path='./Checkpoints/ckpt_best.pth').to(device)
+    class_names = ['license-plate']
 
-device = "cpu"
-model = models.get('yolo_nas_s', num_classes=1, checkpoint_path="./weights/ckpt_best.pth").to(device)
+    result = list(model.predict(image, conf=confidence))[0]
+    bbox_xyxys = result.prediction.bboxes_xyxy.tolist()
+    confidence = result.prediction.confidence
+    labels = result.prediction.labels.tolist()
+    
+    for (bbox_xyxy, confidence, cls) in zip(bbox_xyxys, confidence, labels):
+        bbox = np.array(bbox_xyxy)
+        x1, y1, x2, y2 = bbox[0], bbox[1], bbox[2], bbox[3]
+        x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+        classname = int(cls)
+        class_name = class_names[classname]
+        conf = math.ceil(confidence*100)/100
+        label = f"{class_name}{conf}"
+        text = ocr_image(image, x1, y1, x2, y2)
+        text_size = cv2.getTextSize(label, 0, fontScale=1, thickness=2)[0]
+        background = x1 + text_size[0], y1 - text_size[1] - 3       
+        cv2.rectangle(image, (x1, y1), (x2, y2), (0, 255, 255), 3)
+        cv2.rectangle(image, (x1, y1), background, [255, 144, 20], -1, cv2.LINE_AA)
+        cv2.putText(image, text, (x1, y1-2), 0, 1, [255, 255, 255], thickness=1, lineType=cv2.LINE_AA)
+    st.subheader('Output Image')
+    st.image(image, channels="BGR", use_column_width=1)
 
-class_names = ['license-plate']
-# output = cv2.VideoWriter('./Video/Output.avi', cv2.VideoWriter_fourcc('M', 'J', 'p', 'G'), 10, (frame_width, frame_height))
+def object_detction__ocr_video(video, confidence, kpi1_text, kpi2_text, kpi3_text, stframe):
+    cap = cv2.VideoCapture(video)
 
-while True:
-    ret, frame = cap.read()
-    if ret:
-        result = list(model.predict(frame, conf=0.4))[0]
-        bbox_xyxys = result.prediction.bboxes_xyxy.tolist()
-        confidences = result.prediction.confidence
-        labels = result.prediction.labels.tolist()
-        for (bbox_xyxy, confidence, cls) in zip(bbox_xyxys, confidences, labels):
-            bbox = np.array(bbox_xyxy)
-            x1, y1, x2, y2 = bbox[0], bbox[1], bbox[2], bbox[3]
-            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
-            classname = int(cls)
-            class_name = class_names[classname]
-            conf = math.ceil(confidence*100)/100
-            label = f'{class_name}{conf}'
-            text = ocr_image(frame, x1, y1, x2, y2)
-            label = text
-            print(x1, y1, x2, y2)
-            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 255), 3)
-            text_size = cv2.getTextSize(label, 0, fontScale=1, thickness=2)[0]
-            background = x1 + text_size[0], y1 - text_size[1] - 3
-            cv2.rectangle(frame, (x1, y1), background, [255, 144, 30], -1, cv2.LINE_AA)
-            cv2.putText(frame, label, (x1, y1-2), 0, 1, [255, 255, 255], thickness=1, lineType=cv2.LINE_AA)
-        # output.write(frame)
-        cv2.imshow("Frame", frame)
-        if cv2.waitKey(1) & 0xFF == ord('1'):
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+    frame_width = int(cap.get(3))
+    frame_height = int(cap.get(4))
+    
+    device = "cpu"
+    model = models.get('yolo_nas_s', 
+                        num_classes=1, 
+                        checkpoint_path='./Checkpoints/ckpt_best.pth').to(device)
+    class_names = ['license-plate']
+
+    prev_time = 0
+    while True:
+        ret, frame = cap.read()
+        if ret:
+            result = list(model.predict(frame, conf=0.4))[0]
+            bbox_xyxys = result.prediction.bboxes_xyxy.tolist()
+            confidences = result.prediction.confidence
+            labels = result.prediction.labels.tolist()
+            for (bbox_xyxy, confidence, cls) in zip(bbox_xyxys, confidences, labels):
+                bbox = np.array(bbox_xyxy)
+                x1, y1, x2, y2 = bbox[0], bbox[1], bbox[2], bbox[3]
+                x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+                classname = int(cls)
+                class_name = class_names[classname]
+                conf = math.ceil(confidence*100)/100
+                label = f'{class_name}{conf}'
+                text = ocr_image(frame, x1, y1, x2, y2)
+                label = text
+                cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 255), 3)
+                text_size = cv2.getTextSize(label, 0, fontScale=1, thickness=2)[0]
+                background = x1 + text_size[0], y1 - text_size[1] - 3
+                cv2.rectangle(frame, (x1, y1), background, [255, 144, 30], -1, cv2.LINE_AA)
+                cv2.putText(frame, label, (x1, y1-2), 0, 1, [255, 255, 255], thickness=1, lineType=cv2.LINE_AA)
+            stframe.image(frame, channels='BGR', use_column_width=True)
+            current_time = time.time()
+            fps = 1/(current_time - prev_time)
+            prev_time = current_time
+
+            kpi1_text.write(f"<h1 style='text-align:center; color:red;'>{'{:.1f}'.format(fps)}</h1>", unsafe_allow_html=True)
+            kpi2_text.write(f"<h1 style='text-align:center; color:red;'>{'{:.1f}'.format(width)}</h1>", unsafe_allow_html=True)
+            kpi3_text.write(f"<h1 style='text-align:center; color:red;'>{'{:.1f}'.format(height)}</h1>", unsafe_allow_html=True)
+
+        else:
             break
-    else:
-        break
 
-cap.release()
-cv2.destroyAllWindows()
+    cap.release()
+    cv2.destroyAllWindows()
